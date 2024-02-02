9502e9ba2404910e9d1146d5aeb9ec36d437c346
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-02 09:15:16 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 43320 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> que = new LinkedList<>();
+		for (int i = 1; i < N + 1; i++) que.add(i);
+		
+		boolean throwAway = true;
+		while (que.size() != 1) {
+			if (throwAway) {
+				que.poll();
+				throwAway = false;
+				continue;
+			} else {
+				que.add(que.poll());
+				throwAway = true;
+			}
+		}
+		
+		System.out.println(que.poll());
+		
+		br.close();
+	}
+}
