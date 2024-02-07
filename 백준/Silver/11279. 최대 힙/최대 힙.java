4c6c1736a0bd0b873e883c2cddd21abd3947a073
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-07 14:20:45 +0900

    [Silver II] Title: 최대 힙, Time: 284 ms, Memory: 25484 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
+        
+        for (int i = 0; i < N; i++) {
+        	int x = Integer.parseInt(br.readLine());
+        	
+        	if (x == 0) {
+        		if (que.isEmpty()) sb.append(0).append("\n");
+        		else sb.append(que.poll()).append("\n");
+        	} else {
+        		que.offer(x);
+        	}
+        }
+        
+        System.out.println(sb);
+        br.close();
+	}
+}
