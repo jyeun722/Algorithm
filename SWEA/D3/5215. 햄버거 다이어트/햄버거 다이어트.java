24938f76c7534ff33d8c7d5e6170a4ae2099c905
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-08 09:20:09 +0900

    [D3] Title: 햄버거 다이어트, Time: 179 ms, Memory: 20,140 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int N, L, maxTaste;
+	static int[][] food;
+	
+	static void comb(int cnt, int start, int sumT, int sumK) {
+		if (sumK <= L) {
+			maxTaste = Math.max(sumT, maxTaste);
+		}
+		for (int i = start; i < N; i++) {
+			comb(cnt + 1, i + 1, sumT + food[i][0], sumK + food[i][1]);
+		}
+	}
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc < T + 1; tc++) {
+        	st = new StringTokenizer(br.readLine());
+        	N = Integer.parseInt(st.nextToken());
+        	L = Integer.parseInt(st.nextToken());
+        	maxTaste = 0;
+        	
+        	food = new int[N][2];
+        	for (int i = 0; i < N; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		
+        		food[i][0] = Integer.parseInt(st.nextToken());
+        		food[i][1] = Integer.parseInt(st.nextToken());
+        	}
+        	
+        	comb(0, 0, 0, 0);
+        	
+        	sb.append("#").append(tc).append(" ").append(maxTaste).append("\n");
+        }
+        
+        System.out.println(sb);
+        br.close();
+	}
+}
