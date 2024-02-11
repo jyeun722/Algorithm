0e54675135c8d380be640bd6b7ac7ea972435750
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-11 23:15:48 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 141 ms, Memory: 22,988 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -9,28 +9,45 @@ public class Solution {
     static int[] dx = {0, 1, 0, -1}; // 우, 하, 좌, 상
     static int[] dy = {1, 0, -1, 0};
 
+    static class Node implements Comparable<Node> {
+        int x;
+        int y;
+        int dis;
+
+        public Node(int x, int y, int dis) {
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.dis < o.dis ? -1 : 1;
+        }
+    }
+
     static void bfs(int sum, int x, int y) {
-        PriorityQueue<int[]> que = new PriorityQueue<>(Comparator.comparingInt(i -> i[2]));
-        que.offer(new int[]{x, y, 0});
+        PriorityQueue<Node> que = new PriorityQueue<>();
+        que.offer(new Node(x, y, 0));
         visit[x][y] = true;
 
         while (!que.isEmpty()) {
-            int[] temp = que.poll();
+            Node temp = que.poll();
 
-            if (temp[0] == N - 1 && temp[1] == N - 1) {
-                result = Math.min(result, temp[2]);
+            if (temp.x == N - 1 && temp.y == N - 1) {
+                result = Math.min(result, temp.dis);
                 continue;
             }
 
             for (int d = 0; d < dx.length; d++) {
-                int nextX = temp[0] + dx[d];
-                int nextY = temp[1] + dy[d];
+                int nextX = temp.x + dx[d];
+                int nextY = temp.y + dy[d];
 
                 if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
 
                 if (visit[nextX][nextY]) continue;
 
-                que.offer(new int[]{nextX, nextY, temp[2] + map[nextX][nextY]});
+                que.offer(new Node(nextX, nextY, temp.dis + map[nextX][nextY]));
                 visit[nextX][nextY] = true;
             }
         }
