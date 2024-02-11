4597332e7ef1d1992ad6d05659c2e4889525471d
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-11 22:43:08 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,317 ms, Memory: 103,268 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -7,12 +7,26 @@ public class Solution {
     static double E;
     static int[][] island;
     static boolean[] visit;
+    static int[] parent;
 
-    static void sum() {
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x < y) parent[y] = x;
+        else parent[x] = y;
+    }
+
+    /*
+    static void sumPrim() {
         PriorityQueue<double[]> que = new PriorityQueue<>(Comparator.comparing(i -> (i[2])));
         for (int i = 1; i < N; i++)
-            que.offer(new double[]{0, i, Math.sqrt(Math.pow(island[0][i] - island[0][0], 2)
-                    + Math.pow(island[1][i] - island[1][0], 2))});
+            que.offer(new double[]{0, i, Math.pow(island[0][i] - island[0][0], 2)
+                    + Math.pow(island[1][i] - island[1][0], 2)});
         visit[0] = true;
 
         while (!que.isEmpty()) {
@@ -22,12 +36,33 @@ public class Solution {
 
             if (visit[end]) continue;
             visit[end] = true;
-            result += L * L * E;
+            result += L * E;
 
             for (int i = 1; i < N; i++) {
                 if (i == end || visit[i]) continue;
-                que.offer(new double[]{end, i, Math.sqrt(Math.pow(island[0][i] - island[0][end], 2)
-                        + Math.pow(island[1][i] - island[1][end], 2))});
+                que.offer(new double[]{end, i, Math.pow(island[0][i] - island[0][end], 2)
+                        + Math.pow(island[1][i] - island[1][end], 2)});
+            }
+        }
+    }
+    */
+
+    static void sumKru() {
+        PriorityQueue<double[]> que = new PriorityQueue<>(Comparator.comparing(i -> (i[2])));
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                que.offer(new double[]{i, j, Math.pow(island[0][i] - island[0][j], 2)
+                        + Math.pow(island[1][i] - island[1][j], 2)});
+            }
+        }
+
+        while (!que.isEmpty()) {
+            double[] temp = que.poll();
+            int x = (int) temp[0];
+            int y = (int) temp[1];
+            if (find(x) != find(y)) {
+                union(x, y);
+                result += temp[2] * E;
             }
         }
     }
@@ -42,7 +77,11 @@ public class Solution {
             N = Integer.parseInt(br.readLine()); // 섬의 개수
             result = 0;
 
-            visit = new boolean[N];
+            parent = new int[N]; // kru
+            for (int i = 0; i < N; i++) parent[i] = i;
+
+            visit = new boolean[N]; // prim
+
             island = new int[2][N];
             for (int i = 0; i < 2; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -52,7 +91,8 @@ public class Solution {
             }
 
             E = Double.parseDouble(br.readLine()); // 환경 부담 세율 -> E * L(거리)^2 지불
-            sum();
+//            sumPrim();
+            sumKru();
 
             sb.append("#").append(tc).append(" ").append(Math.round(result)).append("\n");
         }
