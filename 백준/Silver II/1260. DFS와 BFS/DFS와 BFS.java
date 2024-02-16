de4ba5d810d75d0259b0e064706e6f129d5c61a1
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-16 10:48:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 21896 KB -BaekjoonHub

--- 백준/Silver II/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver II/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,71 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void dfs(boolean visit[], LinkedList<Integer>[] list, int v) {
-        visit[v] = true;
-        System.out.print(v + " ");
-
-        Iterator<Integer> it = list[v].iterator();
-        while (it.hasNext()) {
-            int ver = it.next();
-            if (!visit[ver]) dfs(visit, list, ver);
+	static int N, M, V; // 정점 개수, 간선 개수, 탐색 시작 정점 vertex
+	static int[][] map;
+	static StringBuilder sb;
+	static boolean[] visit;
+	
+	static void dfs(int v) {
+		sb.append(v).append(" ");
+		
+		for (int i = 1; i < N + 1; i++) {
+			if (visit[i] || i == v) continue;
+			if (map[i][v] == 1) {
+				visit[i] = true;
+				dfs(i);
+			}
+		}
+	}
+	
+	static void bfs() {
+		ArrayDeque<Integer> que = new ArrayDeque<>();
+		visit[V] = true;
+		que.offer(V);
+		
+		while (!que.isEmpty()) {
+			int ver = que.poll();
+			sb.append(ver).append(" ");
+			
+			for (int i = 1; i < N + 1; i++) {
+				if (visit[i] || i == ver) continue;
+				if (map[i][ver] == 1) {
+					visit[i] = true;
+					que.offer(i);
+				}
+				
+			}
+		}
+	}
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        
+        map = new int[N + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int v = Integer.parseInt(st.nextToken());
+        	int v2 = Integer.parseInt(st.nextToken());
+        	map[v][v2] = map[v2][v] = 1;
         }
-    }
-
-    public static void bfs(boolean visit[], LinkedList<Integer>[] list, int v) {
-        Queue<Integer> que = new LinkedList<>();
-        que.add(v);
-        visit[v] = true;
-
-        while (!que.isEmpty()) {
-            int ver = que.poll();
-            System.out.print(ver + " ");
-            Iterator<Integer> it = list[ver].iterator();
-
-            while (it.hasNext()) {
-                int next = it.next();
-                if (!visit[next]) {
-                    visit[next] = true;
-                    que.add(next);
-                }
-            }
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-
-        LinkedList<Integer>[] list = new LinkedList[n + 1];
-        for (int i = 1; i < n + 1; i++) list[i] = new LinkedList<Integer>();
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list[x].add(y);
-            list[y].add(x);
-        }
-        for (int i = 1; i < n + 1; i++) Collections.sort(list[i]);
-
-        boolean visit[] = new boolean[n + 1];
-        dfs(visit, list, v);
-        System.out.println();
-
-        boolean visit2[] = new boolean[n + 1];
-        bfs(visit2, list, v); 
-    }
+        
+        visit = new boolean[N + 1];
+        visit[V] = true;
+        dfs(V);
+        sb.append("\n");
+        
+        visit = new boolean[N + 1];
+        bfs();
+         
+        System.out.println(sb);
+        br.close();
+	}
 }
