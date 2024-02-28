f7630f0af8883db04d1b5da1f5c540c6396aaa33
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-02-28 17:59:46 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 124 ms, Memory: 22,304 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -4,11 +4,8 @@ import java.util.*;
 public class Solution {
     static int N, SIZE, result, count;
     static int[][] area;
-
     static ArrayList<int[]> cores;
     static int[] order;
-    static boolean[][] ableDfs;
-
     static int[] dx = {0, 1, 0, -1}; // 우, 하, 좌, 상
     static int[] dy = {1, 0, -1, 0};
 
@@ -16,27 +13,22 @@ public class Solution {
         int dis = Integer.MAX_VALUE, minD = -1; // dis: 최소 거리 저장, minD: 최소 거리의 방향 저장
         int x = cores.get(order[i])[0], y = cores.get(order[i])[1];
         for (int d = 0; d < dx.length; d++) {
-            if (!ableDfs[order[i]][d]) continue; // 방문할 필요 없는 방향은 pass
-
             int nextX = x;
             int nextY = y;
             int line = 0;
 
             boolean die = false; // false: 충전선에 닿았으므로 죽지 않음(초기 상태)
-
             while (true) {
                 nextX += dx[d];
                 nextY += dy[d];
 
                 if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) break;
-
-                if (visitArea[nextX][nextY]) { // 다른 선에 닿음
+                if (area[nextX][nextY] == 1 || visitArea[nextX][nextY]) { // 다른 선에 닿음
                     die = true; // true: 비정상적으로 죽음(충전 안됨!)
                     break;
                 }
 
                 line++; // 빈칸 인 경우 선 길이 추가
-
                 if (line > dis) { // 탐색 중 이미 탐색한 다른 방향의 길이보다 커지면 중단
                     die = true; // 충전 안됨!
                     break;
@@ -52,7 +44,6 @@ public class Solution {
             y += dy[minD];
 
             if (x < 0 || y < 0 || x >= N || y >= N) break;
-
             visitArea[x][y] = true;
         }
         return dis;
@@ -118,51 +109,7 @@ public class Solution {
                 }
             }
 
-            int idx = 0;
             SIZE = cores.size(); // core 리스트 크기
-            ableDfs = new boolean[SIZE][4]; // 코어에 따라서 우, 하, 좌, 상 검색할건지 boolean 배열 생성
-
-            // 우, 하, 좌, 상
-            for (int i = 0; i < cores.size(); i++) {
-                int cnt = 4;
-                boolean[] direc = new boolean[4]; // ableDfs에 들어갈 각 4방탐색 여부
-                for (int j = 0; j < 4; j++) direc[j] = true; // 처음엔 다 true로 세팅
-                int x = cores.get(i)[0];
-                int y = cores.get(i)[1];
-                for (int row = 0; row < x; row++) { // 위에 확인
-                    if (area[row][y] == 1) {
-                        direc[3] = false;
-                        cnt--;
-                        break;
-                    }
-                }
-                for (int row = x + 1; row < N; row++) { // 아래 확인
-                    if (area[row][y] == 1) {
-                        direc[1] = false;
-                        cnt--;
-                        break;
-                    }
-                }
-                for (int col = 0; col < y; col++) { // 왼쪽 확인
-                    if (area[x][col] == 1) {
-                        direc[2] = false;
-                        cnt--;
-                        break;
-                    }
-                }
-                for (int col = y + 1; col < N; col++) { // 오른쪽 확인
-                    if (area[x][col] == 1) {
-                        direc[0] = false;
-                        cnt--;
-                        break;
-                    }
-                } // 4방 탐색 돌면서 불가능한 방향은 false로 세팅
-                
-                if (cnt == 0) cores.remove(i--);  // 4방탐색 전부 불가능하면 core 리스트에서 제거
-                else ableDfs[idx++] = direc;
-            }
-
-            SIZE = cores.size(); // 새롭게 core 크기 생성
             order = new int[SIZE]; // core의 탐색 순서를 저장할 배열
 
             comb(0, 0);
