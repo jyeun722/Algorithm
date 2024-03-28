851096324ff1c8be188014432adc1ef9a29176fa
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-03-28 09:54:35 +0900

    [Gold III] Title: 앱, Time: 108 ms, Memory: 12032 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -8,37 +8,32 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken()); // 필요한 메모리 바이트
-        int[][] memory = new int[N + 1][2]; // 바이트, 비용
-
-        int limit = 0;
+        int M = Integer.parseInt(st.nextToken());
+        int[] memory = new int[N + 1];
+        int[] cost = new int[N + 1];
+        
         st = new StringTokenizer(br.readLine());
-        for (int j = 1; j < N + 1; j++) {
-            memory[j][0] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < N + 1; i++) {
+        	memory[i] = Integer.parseInt(st.nextToken());
         }
-
+        
+        int limitCost = 0;
         st = new StringTokenizer(br.readLine());
-        for (int j = 1; j < N + 1; j++) {
-            memory[j][1] = Integer.parseInt(st.nextToken());
-            limit += memory[j][1];
+        for (int i = 1; i < N + 1; i++) {
+        	cost[i] = Integer.parseInt(st.nextToken());
+        	limitCost += cost[i];
         }
-
+        
         int result = Integer.MAX_VALUE;
-        int[][] dp = new int[N + 1][limit + 1];
-        for (int k = 1; k < N + 1; k++) {
-            for (int v = 0; v < limit + 1; v++) { // 가치
-                if (memory[k][1] > v) {
-                    dp[k][v] = dp[k - 1][v];
-                } else {
-                    dp[k][v] = Math.max(dp[k - 1][v], dp[k - 1][v - memory[k][1]] + memory[k][0]);
-                }
-                if (dp[k][v] >= M) {
-                    if (result > v) {
-                        result = v;
-                        limit = v;
-                    }
-                }
-            }
+        int[] dp = new int[limitCost + 1];
+        for (int n = 1; n < N + 1; n++) {
+        	for (int c = limitCost; c - cost[n] >= 0; c--) {
+        		dp[c] = Math.max(dp[c], memory[n] + dp[c - cost[n]]);
+        		if (dp[c] >= M && result > c) {
+        			result = c;
+        			limitCost = c;
+        		}
+        	}
         }
 
         System.out.println(result);
