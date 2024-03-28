9b2e8a781e3d1edeeaed84ccbdf01b7415c03b18
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-03-28 23:27:12 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 343 ms, Memory: 105,760 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int N;
+
+    static int bfs(List<Integer>[] map) {
+        Deque<Integer> que;
+        boolean[] visit;
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            que = new ArrayDeque<>();
+            que.offer(i);
+
+            visit = new boolean[N];
+            visit[i] = true;
+
+            int result = 0, cnt = 0;
+
+            EXIT_WHILE:
+            while (!que.isEmpty()) {
+                int size = que.size();
+                cnt++;
+
+                for (int j = 0; j < size; j++) {
+                    int node = que.poll();
+                    for (int next : map[node]) {
+                        if (visit[next]) continue;
+                        visit[next] = true;
+                        result += cnt;
+
+                        if (result >= answer) break EXIT_WHILE;
+
+                        que.offer(next);
+                    }
+                }
+            }
+            answer = Math.min(result, answer);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        long cur = System.currentTimeMillis();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc < T + 1; tc++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); // 사람 수
+
+            List<Integer>[] map = new ArrayList[N];
+            for (int i = 0; i < N; i++) map[i] = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (Integer.parseInt(st.nextToken()) == 1) {
+                        map[i].add(j);
+                    }
+                }
+            }
+
+            int answer = bfs(map);
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println(after - cur);
+
+
+        System.out.println(sb.toString());
+        br.close();
+    }
+}
