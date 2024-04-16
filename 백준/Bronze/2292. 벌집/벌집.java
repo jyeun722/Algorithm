31861004cff208eb868198792f8f30652274b866
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-04-17 00:15:36 +0900

    [Bronze II] Title: 벌집, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -3,17 +3,14 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
+        int layer = 1;
         
-        int layer = 1, six = 0;
-        
-        if (N != 1) layer++;
-    	for (int i = 2; i < N + 1; i++) {
-    		if (++six == 6 * (layer - 1) + 1) {
-    			layer++;
-    			six = 1;
-    		}
-    	}
+        while (N > 1) {
+        	N -= (6 * layer);
+        	layer++;
+        }
     	System.out.println(layer);
         
         br.close();
