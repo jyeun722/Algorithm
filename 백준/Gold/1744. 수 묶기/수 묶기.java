a4d8be8b9ee8b83667b64d332c2e0570afea3263
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-05-08 08:39:44 +0900

    [Gold IV] Title: 수 묶기, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,36 +1,91 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
+	static PriorityQueue<Integer> pq=new PriorityQueue<Integer>(Collections.reverseOrder());
+	static PriorityQueue<Integer> rpq;
+	static int answer=0;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = Integer.parseInt(br.readLine());
-        int zero = 0, one = 0;
-        ArrayList<Integer> plus = new ArrayList<>();
-        ArrayList<Integer> minus = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-        	int num = Integer.parseInt(br.readLine());
-        	if (num == 0) zero++;
-        	else if (num == 1) one++;
-        	else if (num < 0) minus.add(num);
-        	else plus.add(num);
+        int N=Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++) {
+        	pq.add(Integer.parseInt(br.readLine()));
         }
-        Collections.sort(plus);
-        Collections.sort(minus, Collections.reverseOrder());
-        int sum = 0;
-        
-        for (int i = minus.size() - 1; i > 0; i -= 2) {
-        	sum += minus.get(i) * minus.get(i - 1);
+
+        int fir;
+        int sec;
+        while(!pq.isEmpty()) {
+        	if(pq.size()>=2) {
+        		fir=pq.poll();
+        		sec=pq.poll();
+//        		System.out.println("fir: "+fir);
+//        		System.out.println("sec: "+sec);
+        		if((fir==1||sec==1)&&sec>0) {
+        			answer+=fir;
+        			answer+=sec;
+        			continue;
+        		}
+        		if(fir>0&&sec>0) {
+        			answer+=fir*sec;
+        		}else if(fir>0&&sec==0) {
+        			answer+=fir;
+        			if(pq.size()%2==0) {
+        				answer+=sec;
+        			}else{
+        				pq.add(sec);
+        			}
+        		}
+        		else if(fir>0&&sec<0) {
+        			if(pq.size()%2==0) {
+        				answer+=fir;
+        				answer+=sec;
+        			}else{
+        				answer+=fir;
+        				pq.add(sec);
+        			}
+        			helper();
+        		}
+        		else if(fir==0) {
+        			if(pq.size()%2==0) {
+        				
+        			}else{
+        				pq.add(sec);
+        			}
+        			helper();
+        		}
+        		else if(fir<0) {
+        			pq.add(fir);
+        			pq.add(sec);
+        			helper();
+        		}
+        	}else {
+        		answer+=pq.poll();
+        	}
         }
-        if (minus.size() % 2 != 0 && zero < 1) sum += minus.get(0);
         
-        for (int i = plus.size() - 1; i > 0; i -= 2) {
-        	sum += plus.get(i) * plus.get(i - 1);
+        System.out.println(answer);
+    }
+    static void helper() {
+    	rpq=new PriorityQueue<Integer>();
+    	// 들어왔으면 음수만 남음
+    	while(!pq.isEmpty()) {
+			rpq.add(pq.poll());
+		}
+    	int fir;
+    	int sec;
+        while(!rpq.isEmpty()) {
+        	if(rpq.size()>=2) {
+        		fir=rpq.poll();
+        		sec=rpq.poll();
+//        		System.out.println("fir: "+fir);
+//        		System.out.println("sec: "+sec);
+        		answer+=fir*sec;
+        	}else {//1개 남음
+        		answer+=rpq.poll();
+        		break;
+        	}
         }
-        if (plus.size() % 2 != 0) sum += plus.get(0);
-        
-        System.out.println(sum + one);
-        br.close();
-	}
+    }
 }
