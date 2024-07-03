0d081ae7838834d9de9f1e3300ceda26ed1b24fb
Yeeun Jeong <85361813+jyeun722@users.noreply.github.com>
2024-07-03 21:53:34 +0900

    Update 여행 가자.java

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -17,44 +17,6 @@ public class Main {
         return find(parent[x], parent);
     }
 
-    static boolean bfs(int[][] graph, int x, int y) {
-        Deque<Integer> que = new ArrayDeque<>();
-        que.offer(x);
-
-        boolean[] visit = new boolean[N];
-        visit[x] = true;
-
-        while (!que.isEmpty()) {
-            int city = que.poll();
-            if (city == y) return true;
-
-            for (int i = 0; i < N; i++) {
-                if (visit[i]) continue;
-                visit[i] = true;
-
-                if (graph[city][i] == 0) continue;
-                que.offer(i);
-            }
-        }
-        return false;
-    }
-
-    static boolean cityCheck(int[][] graph, int[] cities, boolean[][] visited) {
-        for (int i = 0; i < cities.length - 1; i++) {
-            // 방문 가능한 곳이면 pass
-            if (visited[cities[i]][cities[i + 1]]) continue;
-
-            // 방문 안해봤던 곳
-            if (bfs(graph, cities[i], cities[i + 1])) {
-                visited[cities[i]][cities[i + 1]] = true;
-                visited[cities[i + 1]][cities[i]] = true;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
@@ -74,18 +36,12 @@ public class Main {
             }
         }
 
-//        System.out.println(Arrays.toString(parent));
-//        for (int i = 0; i < N; i++) {
-//            System.out.println(Arrays.toString(graph[i]));
-//        }
-
         int[] cities = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int city = Integer.parseInt(st.nextToken()) - 1;
             cities[i] = city;
         }
-//        System.out.println(Arrays.toString(cities));
 
         for (int i = 0; i < N; i++) {
             parent[i] = find(i, parent);
@@ -106,14 +62,6 @@ public class Main {
         } else {
             System.out.println("NO");
         }
-
-
-        boolean[][] visited = new boolean[N][N];
-//        if (cityCheck(graph, cities, visited)) {
-//            System.out.println("YES");
-//        } else {
-//            System.out.println("NO");
-//        }
 
         br.close();
     }
